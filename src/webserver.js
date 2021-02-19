b413d4ed5249b0cf342bba370d94d498d00e7cd6
lebeur_a <lebeur_a@epita.fr>
2021-02-19 18:09:24 +0000

    Add door handeling

--- src/webserver.js
+++ src/webserver.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const data = require('./data.json'); //FIXME replace with database
+const door = require('./door.json');
 
 app.use(express.json());
 
@@ -19,6 +20,24 @@ app.post('/data', (req, res) => {
   data.push(req.body);
   fs.writeFile("./data.json", JSON.stringify(data), function(err) { if (err) { return console.log(err); } console.log("Filed saved");}); //FIXME add row in database
   res.status(200).json(data);
+});
+
+app.get('/door', (req, res) => {
+  res.status(200).json(door);
+});
+
+app.get('/door/open', (req, res) => {
+  console.log("Opening door");
+  door.push(JSON.parse('{"timestamp": "now", "status": "open"}'));
+  //FIXME insert in database instead
+  res.status(200).json();
+});
+
+app.get('/door/close', (req, res) => {
+  console.log("Closing door");
+  door.push(JSON.parse('{"timestamp": "now", "status": "close"}'));
+  //FIXME insert in database with timestamp
+  res.status(200).json();
 });
 
 app.listen(8080, () => {
