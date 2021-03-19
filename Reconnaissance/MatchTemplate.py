f6f690a2b7327be0fe91463994194406b32c9abb
Jean-Victor <jeanvictor.gandossi@gmail.com>
2021-03-19 08:36:53 +0100

    Jean-Victor first commit

---
+++ Reconnaissance/MatchTemplate.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+#importation de l'image de base et l'image a rechercher
+image = cv2.imread('poules.jpg')
+template = cv2.imread('poule.jpeg')
+
+
+#Conversion en nuance de gris
+imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+#Retrouver l'image template dans l'image de base
+result = cv2.matchTemplate(imageGray,templateGray, cv2.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+top_left = max_loc
+h,w = templateGray.shape
+loc=np.where(result >=0.7)
+for i in zip(*loc[::-1]):
+    cv2.rectangle(image,i, (i[0] + w, i[1] + h),(0,0,255),1)
+
+#Affichage des resultats
+cv2.imshow("Template", template)
+cv2.imshow("Result", image)
+
+cv2.moveWindow("Template", 10, 50)
+cv2.moveWindow("Result", 150, 50)
+
+cv2.waitKey(0)
